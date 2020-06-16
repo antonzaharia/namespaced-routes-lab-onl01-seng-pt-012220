cd99daf6f2fc9506b04f17d0fb3424dd118708fe
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 20:03:35 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -6,14 +6,13 @@ class Admin::PreferencesController < ApplicationController
   def change
     @pref = Admin::Preference.find_by(id: "1")
     if params[:change_artist]
-      binding.pry
-      # if @pref.allow_create_artists == "true"
-      #   @pref.update(allow_create_artists: "false")
-      #   @pref.save
-      # else
-      #   @pref.update(allow_create_artists: "true")
-      #   @pref.save
-      # end
+      if @pref.allow_create_artists == "true"
+        @pref.update(allow_create_artists: false)
+        @pref.save
+      else
+        @pref.update(allow_create_artists: true)
+        @pref.save
+      end
       redirect_to admin_preferences_path, alert: "Users can create artists: #{@pref.allow_create_artists}."
     end
   end
