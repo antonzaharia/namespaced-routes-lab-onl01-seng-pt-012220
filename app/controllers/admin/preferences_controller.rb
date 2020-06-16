a7c5cdaecfedef6f1a4559fd4d7f610528e10eec
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 20:07:01 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -6,7 +6,7 @@ class Admin::PreferencesController < ApplicationController
   def change
     @pref = Admin::Preference.find_by(id: "1")
     if params[:change_artist]
-      @prefchange_artist
+      @pref.change_artist
       redirect_to admin_preferences_path, alert: "Users can create artists: #{@pref.allow_create_artists}."
     end
   end
