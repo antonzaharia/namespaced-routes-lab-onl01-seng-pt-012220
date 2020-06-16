6f7363fc1504567fb1c14e571a4b8bfbbd0cec30
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 20:35:46 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -4,12 +4,15 @@ class Admin::PreferencesController < ApplicationController
     @pref = Admin::Preference.find_by(id: "1")
   end
 
-  def change
+  def change_artist
     @pref = Admin::Preference.find_by(id: "1")
     if params[:change_artist]
       @pref.change_artist
       redirect_to admin_preferences_path, alert: "Users can create artists: #{@pref.allow_create_artists}."
-    elsif params[:change_song]
+    end
+  end
+  def change_song
+    if params[:change_song]
       @pref.change_artist
       redirect_to admin_preferences_path, alert: "Users can create songs: #{@pref.allow_create_artists}."
     end
