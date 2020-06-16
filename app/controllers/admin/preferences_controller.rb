44aa4f30f8e709887247bb707fb420f2b3462803
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 19:47:43 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -7,6 +7,7 @@ class Admin::PreferencesController < ApplicationController
     @pref = Admin::Preference.find_by(id: "1")
     if params[:change_artist]
       @pref.allow_create_artists = !@pref.allow_create_artists
+      redirect_to admin_preferences_path, alert: "Artists allwoances is now #{@pref.allow_create_artists}."
     end
   end
 end
