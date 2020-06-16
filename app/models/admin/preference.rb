3d76fbb4c3cd3c523a1f3f24a6276be0bacd6e34
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 20:05:11 +0000

    Automatically backed up by Learn

--- app/models/admin/preference.rb
+++ app/models/admin/preference.rb
@@ -1,12 +1,12 @@
 class Admin::Preference < ActiveRecord::Base
 
-  def change_artist
-    if @pref.allow_create_artists == true
-      @pref.update(allow_create_artists: false)
-      @pref.save
+  def change_artist(pref)
+    if pref.allow_create_artists == true
+      pref.update(allow_create_artists: false)
+      pref.save
     else
-      @pref.update(allow_create_artists: true)
-      @pref.save
+      pref.update(allow_create_artists: true)
+      pref.save
     end
   end
 end
