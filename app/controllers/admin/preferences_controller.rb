cd7f48e41b6922e6aca5fa63bb39608dfd930eab
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-16 19:42:10 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -6,7 +6,7 @@ class Admin::PreferencesController < ApplicationController
   def change
     @pref = Preference.find_by(id: "1")
     if params[:change_artist]
-      @pref.allow_create_artist.boolean = !@pref.allow_create_artist.boolean
+      @pref.allow_create_artist = !@pref.allow_create_artist
     end
   end
 end
